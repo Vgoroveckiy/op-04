b5b6e0a0f106a341afa331e322ed3ac20e7b188c
Vgorovecky <vgoroveckiy@gmail.com>
2025-04-10 20:13:31 +0300

    11

--- op04-1.py
+++ op04-1.py
@@ -1,7 +1,11 @@
-def sum_range(start, end):
-  sum = 0
-  for i in range(start, end+1):
-    sum += i
-  return sum
+# Напишите функцию **sum_range(start, end)**, которая суммирует все целые числа от значения **start** до величины **end** включительно.
 
-print(sum_range(1,5))
+
+def sum_range(start, end):
+    sum = 0
+    for i in range(start, end + 1):
+        sum += i
+    return sum
+
+
+print(sum_range(1, 5))
