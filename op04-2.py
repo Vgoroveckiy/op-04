7fcd7986ea0490a68e1f0c8ac28ec46bdcc62cdd
Vgorovecky <vgoroveckiy@gmail.com>
2025-04-10 20:15:53 +0300

    1

--- op04-2.py
+++ op04-2.py
@@ -1,9 +1,13 @@
+# Написать функцию square, принимающую 1 аргумент — сторону квадрата, и возвращающую 3 значения (с помощью [кортежа](https://pythonworld.ru/tipy-dannyx-v-python/kortezhi-tuple.html), после return перечислить все значения через запятую): периметр квадрата, площадь квадрата и диагональ квадрата.
+
 import math
 
+
 def square(x):
-  perimetr = x * 4
-  ploshad = x * x
-  diagonal = x * math.sqrt(2)
-  return (perimetr, ploshad, diagonal)
+    perimetr = x * 4
+    ploshad = x * x
+    diagonal = x * math.sqrt(2)
+    return (perimetr, ploshad, diagonal)
+
 
 print(square(5))
